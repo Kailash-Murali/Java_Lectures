fe0c5dc93d4c26bb8ff18e0dc362df0167ce5132
kailashmurali16 <k080171@psbbschools.edu.in>
2024-07-28 20:17:08 +0530

    Add existing file

---
+++ JavaLec.java
@@ -0,0 +1,80 @@
+//import java.util.Scanner;
+
+public class JavaLec {
+
+    // static void Calculator() {
+
+    //     int Choice = 1;
+
+    //     System.out.println("\nBasic Calculator.\n\nWelcome User!! Given calculator performs basic arithmetic operations for two operands.");
+    //     System.out.println("Choose operation by giving the serial no. assigned, \n 1. Add\n 2. Sub\n 3. Mul\n 4. Div\n 5. Power");
+
+    //     while (Choice == 1) {
+
+    //         float Op1, Op2;
+    //         int Op;
+    //         Scanner Scn = new Scanner(System.in);
+
+    //         try {
+
+    //             System.out.println("\nEnter operation no: ");
+    //             Op = Scn.nextInt();
+
+    //             System.out.println("\nEnter operands, successively: ");
+    //             Op1 = Scn.nextFloat();
+    //             Op2 = Scn.nextFloat();
+
+    //             if (Op == 1)
+    //                 System.out.printf("\nThe output is: %f\n", Op1 + Op2);
+    //             else if (Op == 2)
+    //                 System.out.printf("\nThe output is: %f\n", Op1 - Op2);
+    //             else if (Op == 3)
+    //                 System.out.printf("\nThe output is: %f\n", Op1 * Op2);
+    //             else if (Op == 4) {
+    //                 if (Op2 != 0)
+    //                     System.out.printf("\nThe output is: %f\n", Op1 / Op2);
+    //                 else {
+    //                     System.out.println("\nDivision by zero not possible! Try again with valid inputs!");
+    //                     continue;
+    //                 }
+    //             } else if (Op == 5)
+    //                 System.out.printf("\nThe output is: %f\n", Math.pow(Op1, Op2));
+    //             else {
+    //                 System.out.println("\nInvalid input! Try again with valid inputs! ");
+    //                 continue;
+    //             }
+
+    //         } 
+    //         catch (Exception e) {
+
+    //             System.out.println("\nError encountered! Try again!");
+    //             System.out.println(e);
+    //             continue;
+
+    //         }
+
+    //         System.out.println("\nDo you wish to continue? [1/else]: ");
+    //         Choice = Scn.nextInt();
+
+    //         if (Choice == 1)
+    //             continue;
+    //         else {
+    //             System.out.println("\nThank You!");
+    //             Scn.close();
+    //             break;
+    //         }
+
+    //     }
+    // } 
+    // static void Math(){
+	// 	int x = 10;
+	// 	int y = 20;
+	// 	System.out.println(Math.random());
+	// }
+
+    public static void main(String[] args) {
+        // Math();
+        // Calculator();
+        // system.out.println("Hello World!");
+    }
+}
